446f91bbe1d0cb868e984e7012f6d7f4ed2e6948
justintham <81354747+justintham@users.noreply.github.com>
2022-04-03 23:16:00 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ceygv0gzptju.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "cqz-employee"
+customregion = "us-east-1"
