0824d1b0aec54e25bddf9ca57ed0d9549f17f3cf
KJYong777 <101789600+KJYong777@users.noreply.github.com>
2022-04-08 22:26:38 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-customhost = "employee.cjvcbowyawpi.us-east-1.rds.amazonaws.com"
+customhost = "employee.cqnylupfswyh.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "bait3273"
 customdb = "employee"
-custombucket = "justinthamjiawei-employee"
+custombucket = "yongkangjeff-employee"
 customregion = "us-east-1"
 cloudfront = "dgyy4raw0tup7.cloudfront.net"
-
 
