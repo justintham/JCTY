811bdcc5be8ef220e0ad517bb28cad712fe0b497
TAN-CHEE-ONN <77651630+TAN-CHEE-ONN@users.noreply.github.com>
2022-04-10 10:49:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-customhost = "employee.cqnylupfswyh.us-east-1.rds.amazonaws.com"
+customhost = "employee.cenumlmfsqnw.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "bait3273"
+custompass = "Bait3273"
 customdb = "employee"
-custombucket = "yongkangjeff-employee"
+custombucket = "tancheeonn-bucket"
 customregion = "us-east-1"
-cloudfront = "dgyy4raw0tup7.cloudfront.net"
+cloudfront = "d2axm6kw0u8vqw.cloudfront.net"
 
